e7d077d4ba9f4d5578b0120edc2075fd360cba36
Mihai Darie <mihaidarie@microsoft.com>
2025-03-17 12:42:56 +0200

    added waterfall example

---
+++ OrderHistory.cs
@@ -0,0 +1,21 @@
+﻿namespace order_management_waterfall;
+
+internal class OrderHistory
+{
+    private readonly List<Order> orders = new List<Order>();
+
+    public void AddOrder(Order order)
+    {
+        orders.Add(order);
+    }
+    
+    public void RemoveOrder(int orderId)
+    {
+        orders.RemoveAll(o => o.OrderId == orderId);
+    }
+    
+    public List<Order> GetOrderHistory()
+    {
+        return orders;
+    }
+}

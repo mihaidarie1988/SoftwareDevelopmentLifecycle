7f8b37777bdaad8211b4b7c6a0d30dc15bd43ce4
Mihai Darie <mihaidarie@microsoft.com>
2025-03-17 13:11:52 +0200

    prepared scrum samples - iteration 4 (=waterfall)

---
+++ order-management-scrum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace order_management_scrum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        OrderManagement orderManagement = new OrderManagement();
+
+        // PlaceOrder - 2 times
+        orderManagement.PlaceOrder(1, "Laptop", 2);
+        orderManagement.PlaceOrder(2, "Mouse", 5);
+
+        // ViewOrderHistory
+        orderManagement.ViewOrderHistory();
+
+        // ModifyOrder
+        orderManagement.ModifyOrder(1, "Gaming Laptop", 1);
+
+        // RemoveOrder
+        orderManagement.RemoveOrder(2);
+
+        // ViewOrderHistory again to see the changes
+        orderManagement.ViewOrderHistory();
+    }
+}
